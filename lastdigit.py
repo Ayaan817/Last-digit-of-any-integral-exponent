c40efbcf02d6b9c87292629037b15498383d5856
anonymanus817 <73923995+Ayaan817@users.noreply.github.com>
2021-06-22 12:04:40 +0530

    Create lastdigit.py

---
+++ lastdigit.py
@@ -0,0 +1,43 @@
+import math as mt
+
+num = int(input("Enter number: "))
+lastdigit_num = int(repr(num)[-1])
+
+exp = int(input("Enter exponent: "))
+lastdigit_exp = int(repr(exp)[-1])
+last2digit_exp = 10*int(repr(exp)[-2])+int(repr(exp)[-1])
+mod_exp_2_3_7_8 = last2digit_exp % 4
+
+if lastdigit_exp % 2 == 1:
+    mod_exp_4_9 = 1
+else:
+    mod_exp_4_9 = 0
+
+arr_2 = [2,4,8,6]
+arr_3 = [3,9,7,1]
+arr_4 = [4,6]
+arr_7 = [7,9,3,1]
+arr_8 = [8,4,2,6]
+arr_9 = [9,1]
+
+if lastdigit_num == 0:
+    print(f"Last digit of {num}^{exp} is 0.")
+
+elif lastdigit_num == 1:
+    print(f"Last digit of {num}^{exp} is 1.")
+
+elif lastdigit_num == 2:
+    print(f"Last digit of {num}^{exp} is {arr_2[mod_exp_2_3_7_8 - 1]}.")
+    
+elif lastdigit_num == 3:
+    print(f"Last digit of {num}^{exp} is {arr_3[mod_exp_2_3_7_8 - 1]}.")
+        
+        
+        
+'''
+incomplete
+'''
+        
+        
+        
+        
